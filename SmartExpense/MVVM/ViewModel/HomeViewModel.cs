3d0bac830ebea85aa4ba325ecda06b0fdb216418
Vitalik Barabash <vitalik.barabash.2002@gmail.com>
2023-05-05 17:01:41 +0300

    Несуттєві зміни в форматі класу. Виконано просте тестування роботи із отриманими даними, що можуть легко замінитись базою даних.

--- SmartExpense/MVVM/ViewModel/HomeViewModel.cs
+++ SmartExpense/MVVM/ViewModel/HomeViewModel.cs
@@ -1,68 +1,96 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using LiveCharts;
 using LiveCharts.Defaults;
 using LiveCharts.Wpf;
 using SmartExpense.Core;
+using SmartExpense.Core.Database.DataModels;
 
 namespace SmartExpense.MVVM.ViewModel;
 
 public class HomeViewModel : ObservableObject
 {
-    public SeriesCollection IncomeOutcomeSeriesCollection { get; set; }
+    public SeriesCollection PieSeriesCollection { get; set; }
 
     private decimal _income;
     private decimal _outcome;
     private decimal _balance;
-    
-    public object? Income
+
+    private List<Transaction> _transactionsList;
+
+    public decimal Income
     {
         get => _income;
         set
         {
-            _income = (decimal)value!;
+            _income = value!;
             OnPropertyChanged();
         }
     }
     
-    public object? Outcome
+    public decimal Outcome
     {
         get => _outcome;
         set
         {
-            _outcome = (decimal)value!;
+            _outcome = value!;
             OnPropertyChanged();
         }
     }
     
-    public object? Balance
+    public decimal Balance
     {
         get => _balance;
         set
         {
-            _balance = (decimal)value!;
+            _balance = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    public List<Transaction> TransactionList
+    {
+        get => _transactionsList;
+        set
+        {
+            _transactionsList = value;
             OnPropertyChanged();
         }
     }
 
     public HomeViewModel()
     {
-        _income = 1000.0M;
-        _outcome = 435.0M;
-        _balance = 17000.20M;
+        var transactions = new List<Transaction>
+        {
+            new() { OwnerId = 10000001, Amount = 100.20M, Type = "Витрата", CreationDate = DateTime.Today.Date, Discription = "Buy some drink", AccountTitle = "Credit Card"},
+            new() { OwnerId = 10000001, Amount = 1290.80M, Type = "Надходження", CreationDate = DateTime.Today.Date, Discription = "Wage", AccountTitle = "Card"},
+            new() { OwnerId = 10000002, Amount = 100.20M, Type = "Надходження", CreationDate = DateTime.Today.Date, Discription = "But some drink", AccountTitle = "Credit Card"},
+            new() { OwnerId = 10000003, Amount = 100.20M, Type = "Витрата", CreationDate = DateTime.Today.Date, AccountTitle = "Credit Cart"},
+            new() { OwnerId = 10000001, Amount = 10.0M, Type = "Витрата", CreationDate = DateTime.Today.Date, Discription = "Tic-tac", AccountTitle = "Wallet"},
+            new() { OwnerId = 10000001, Amount = 100.20M, Type = "Надходження", CreationDate = DateTime.Today.Date, Discription = "But some drink", AccountTitle = "Card"},
+        };
         
-        IncomeOutcomeSeriesCollection = new SeriesCollection
+        var userTransactions = transactions.Where(x => x.OwnerId == 10000001).ToList();
+
+        Income = userTransactions.Where(x => x.Type == "Надходження").Sum(x=> x.Amount);
+        Outcome = userTransactions.Where(x => x.Type == "Витрата").Sum(x=> x.Amount);
+        Balance = Income - Outcome;
+        
+        TransactionList = userTransactions;
+
+        PieSeriesCollection = new SeriesCollection
         {
             new PieSeries
             {
                 Title = "Надходження",
-                Values = new ChartValues<ObservableValue>{ new((double)_income) },
+                Values = new ChartValues<ObservableValue> { new((double)Income) },
                 DataLabels = true
             },
             new PieSeries
             {
                 Title = "Витрати",
-                Values = new ChartValues<ObservableValue>{ new((double)_outcome) },
+                Values = new ChartValues<ObservableValue> { new((double)Outcome) },
                 DataLabels = true
             }
         };
