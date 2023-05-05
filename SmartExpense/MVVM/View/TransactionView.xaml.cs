22361f09fb772a916453e68b2b4da44456941de0
Vitalik Barabash <vitalik.barabash.2002@gmail.com>
2023-05-05 17:00:23 +0300

    Підготовлено інтерфейс сторінки транзакцій.

--- SmartExpense/MVVM/View/TransactionView.xaml.cs
+++ SmartExpense/MVVM/View/TransactionView.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using SmartExpense.Core.Database.DataModels;
 
 namespace SmartExpense.MVVM.View;
 
@@ -7,5 +13,11 @@ public partial class TransactionView : UserControl
     public TransactionView()
     {
         InitializeComponent();
+    }
+    
+    /* ToDo: розробити логіку наскрізного пошуку. */
+    private void SearchBox_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        
     }
 }
--- SmartExpense/MVVM/ViewModel/TransactionViewModel.cs
+++ SmartExpense/MVVM/ViewModel/TransactionViewModel.cs
@@ -1,6 +1,38 @@
-﻿namespace SmartExpense.MVVM.ViewModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SmartExpense.Core;
+using SmartExpense.Core.Database.DataModels;
 
-public class TransactionViewModel
+namespace SmartExpense.MVVM.ViewModel;
+
+public class TransactionViewModel : ObservableObject
 {
+    private List<Transaction> _transactionsList;
     
+    public List<Transaction> TransactionList
+    {
+        get => _transactionsList;
+        set
+        {
+            _transactionsList = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    public TransactionViewModel()
+    {
+        var transactions = new List<Transaction>
+        {
+            new() { OwnerId = 10000001, Amount = 100.20M, Type = "Витрата", CreationDate = DateTime.Today.Date, Discription = "Buy some drink", AccountTitle = "Credit Card"},
+            new() { OwnerId = 10000001, Amount = 1290.80M, Type = "Надходження", CreationDate = DateTime.Today.Date, Discription = "Wage", AccountTitle = "Card"},
+            new() { OwnerId = 10000002, Amount = 100.20M, Type = "Надходження", CreationDate = DateTime.Today.Date, Discription = "But some drink", AccountTitle = "Credit Card"},
+            new() { OwnerId = 10000003, Amount = 100.20M, Type = "Витрата", CreationDate = DateTime.Today.Date, AccountTitle = "Credit Cart"},
+            new() { OwnerId = 10000001, Amount = 10.0M, Type = "Витрата", CreationDate = DateTime.Today.Date, Discription = "Tic-tac", AccountTitle = "Wallet"},
+            new() { OwnerId = 10000001, Amount = 100.20M, Type = "Надходження", CreationDate = DateTime.Today.Date, Discription = "But some drink", AccountTitle = "Card"},
+        };
+        
+        var userTransactions = transactions.Where(x => x.OwnerId == 10000001).ToList();
+        TransactionList = userTransactions;
+    }
 }
