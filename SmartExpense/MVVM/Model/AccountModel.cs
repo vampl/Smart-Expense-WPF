cdd3aa1b8ffb677e5edc1efab7ce2f830bbd1c68
Vitalik Barabash <vitalik.barabash.2002@gmail.com>
2023-05-06 20:17:56 +0300

    Невеличкі зміни в моделях даних, несуттєві.

--- SmartExpense/MVVM/Model/AccountModel.cs
+++ SmartExpense/MVVM/Model/AccountModel.cs
@@ -4,11 +4,10 @@ namespace SmartExpense.MVVM.Model;
 
 public class AccountModel
 {
+    [Required] public UserModel? User { get; set; }
     public long Id { get; set; }
-    public long OwnerId { get; set; }
     public string Title { get; set; }
     public string Type { get; set; }
     public string Discription { get; set; }
     public decimal Amount { get; set; }
-    [Required] public UserModel? User { get; set; }
 }
--- SmartExpense/MVVM/Model/TransactionModel.cs
+++ SmartExpense/MVVM/Model/TransactionModel.cs
@@ -5,12 +5,11 @@ namespace SmartExpense.MVVM.Model;
 
 public class TransactionModel
 {
+    [Required] public UserModel? User { get; set; }
     public long Id { get; set; }
-    public long OwnerId { get; set; }
     public decimal Amount { get; set; }
     public string Type { get; set; }
     public DateTime CreationDate { get; set; }
     public string? Discription { get; set; }
     public string AccountTitle { get; set; }
-    [Required] public UserModel? User { get; set; }
 }
