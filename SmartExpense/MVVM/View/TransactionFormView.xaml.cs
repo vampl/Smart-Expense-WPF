0e017b220bd475487a26b72a7dbbef9451b126a1
Vitalik Barabash <vitalik.barabash.2002@gmail.com>
2023-05-06 20:21:23 +0300

    Форма по створенню транзакцій її ініціалізація та базовий функціонал.

---
+++ SmartExpense/MVVM/View/TransactionFormView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace SmartExpense.MVVM.View;
+
+public partial class TransactionFormView : Window
+{
+    public TransactionFormView()
+    {
+        InitializeComponent();
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
---
+++ SmartExpense/MVVM/ViewModel/TransactionFormViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SmartExpense.Core;
+
+namespace SmartExpense.MVVM.ViewModel;
+
+public class TransactionFormViewModel
+{
+    private ApplicationContext _context;
+
+    public List<string> TransactionTypes { get; set; }
+    public List<string> AccountsTitle { get; set; }
+
+    public TransactionFormViewModel()
+    {
+        TransactionTypes = new List<string>
+        {
+            new string("Надходження"),
+            new string("Витрата")
+        };
+
+        _context = new ApplicationContext();
+        AccountsTitle = _context.Accounts.Where(x => x.User.UserName == "10000001").Select(x => x.Title).ToList();
+    }
+}
