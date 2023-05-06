73cea2c309806e0dfb464a2ff2da980156fbdc4f
Vitalik Barabash <vitalik.barabash.2002@gmail.com>
2023-05-06 20:18:35 +0300

    Заповнення таблиці рахунків та видалення їх із таблиці (без бази даних).

--- SmartExpense/MVVM/ViewModel/AccountViewModel.cs
+++ SmartExpense/MVVM/ViewModel/AccountViewModel.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using SmartExpense.Core;
-using SmartExpense.Core.Database;
 using SmartExpense.MVVM.Model;
 
 namespace SmartExpense.MVVM.ViewModel;
@@ -9,20 +9,50 @@ namespace SmartExpense.MVVM.ViewModel;
 public class AccountViewModel : ObservableObject
 {
     private ApplicationContext _context;
-    private List<AccountModel> _accountsList;
-    
-    public List<AccountModel> AccountsList
+
+    private ObservableCollection<AccountModel> _accounts;
+
+    public ObservableCollection<AccountModel> Accounts
     {
-        get => _accountsList;
+        get => _accounts;
         set
         {
-            _accountsList = value;
+            _accounts = value;
             OnPropertyChanged();
         }
     }
+
+    private RelayCommand _mRowRemoveCommand;
+    public RelayCommand RemoveCommand
+    {
+        get
+        {
+            if (_mRowRemoveCommand == null)
+            {
+                _mRowRemoveCommand = new RelayCommand(RemoveRow, CanRemoveRow);
+            }
+            return _mRowRemoveCommand;
+        }
+    }
+    private void RemoveRow(object parameter)
+    {
+        var index = Accounts.IndexOf(parameter as AccountModel);
+        if (index > -1 && index < Accounts.Count)
+        {
+            Accounts.RemoveAt(index);
+        }
+    }
     
+    private bool CanRemoveRow(object parameter)
+    {
+        return true;
+    }
+
     public AccountViewModel()
     {
         _context = new ApplicationContext();
+        
+        var userAccounts = _context.Accounts.Where(x => x.User.UserName == "10000001").ToList();
+        Accounts = new ObservableCollection<AccountModel>(userAccounts);
     }
 }
