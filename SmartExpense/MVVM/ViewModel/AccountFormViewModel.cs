ea5b49201a23faa278f6cf7ee633e76b15df80e9
Vitalik Barabash <vitalik.barabash.2002@gmail.com>
2023-05-07 21:03:36 +0300

    Механізм створення та додання в таблицю нового рахунку. Збереження в базу даних не реалізовано.

---
+++ SmartExpense/MVVM/ViewModel/AccountFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using SmartExpense.MVVM.Model;
+
+namespace SmartExpense.MVVM.ViewModel;
+
+public class AccountFormViewModel : AccountModel
+{
+    private const string TEXT_CASH_ACCOUNT_DEFINOTION = "Готівка";
+    private const string TEXT_CARD_ACCOUNT_DEFINOTION = "Карта";
+    
+    public List<string> AccountsTypes { get; set; }
+
+    public AccountFormViewModel()
+    {
+        AccountsTypes = new List<string> { TEXT_CASH_ACCOUNT_DEFINOTION, TEXT_CARD_ACCOUNT_DEFINOTION };
+    }
+}
