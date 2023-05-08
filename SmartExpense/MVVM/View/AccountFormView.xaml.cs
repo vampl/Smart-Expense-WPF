500f9513e08ac499188d6cf470fbb7d425a2ca0e
Vitalik Barabash <vitalik.barabash.2002@gmail.com>
2023-05-08 21:50:02 +0300

    Форма додавання нових рахунків та її логіка.

--- SmartExpense/MVVM/View/AccountFormView.xaml.cs
+++ SmartExpense/MVVM/View/AccountFormView.xaml.cs
@@ -1,40 +1,11 @@
-﻿using System;
-using System.Windows;
-using SmartExpense.MVVM.Model;
+﻿using System.Windows;
 
 namespace SmartExpense.MVVM.View;
 
 public partial class AccountFormView : Window
 {
-    private Action<AccountModel> _callback;
-    
-    public AccountFormView(Action<AccountModel> callback)
+    public AccountFormView()
     {
         InitializeComponent();
-        _callback = callback;
-    }
-
-    private void AddRowButton_OnClick(object sender, RoutedEventArgs e)
-    {
-        if (isCorrectData())
-        {
-            _callback(new AccountModel
-            {
-                Title = TitleBox.Text,
-                Type = TypeComboBox.SelectionBoxItem.ToString()!,
-                Description = DescriptionTextBox.Text,
-                Amount = Convert.ToDecimal(AmountTextBox.Text),
-            });
-            
-            Close();
-        }
-        else {
-            MessageBox.Show("Перевірте введені дані!");
-        }
-    }
-    
-    private bool isCorrectData()
-    {
-        return decimal.TryParse(AmountTextBox.Text, out var amount);
     }
 }
--- SmartExpense/MVVM/ViewModel/AccountFormViewModel.cs
+++ SmartExpense/MVVM/ViewModel/AccountFormViewModel.cs
@@ -1,14 +1,78 @@
 ﻿using System.Collections.Generic;
+using SmartExpense.Core;
 using SmartExpense.MVVM.Model;
 
 namespace SmartExpense.MVVM.ViewModel;
 
-public class AccountFormViewModel : AccountModel
+public class AccountFormViewModel : ObservableObject
 {
     private const string TEXT_CASH_ACCOUNT_DEFINOTION = "Готівка";
     private const string TEXT_CARD_ACCOUNT_DEFINOTION = "Карта";
     
     public List<string> AccountsTypes { get; set; }
+    
+    private string _title;
+    public string Title
+    {
+        get => _title;
+        set
+        {
+            _title = value;
+            OnPropertyChanged();
+        } 
+    }
+    
+    private string _type;
+    public string Type
+    {
+        get => _type;
+        set
+        {
+            _type = value;
+            OnPropertyChanged();
+        } 
+    }
+
+    private string _description;
+    public string Description
+    {
+        get => _description;
+        set
+        {
+            _description = value;
+            OnPropertyChanged();
+        } 
+    }
+    
+    private decimal _amount;
+    public decimal Amount
+    {
+        get => _amount;
+        set
+        {
+            _amount = value;
+            OnPropertyChanged();
+        } 
+    }
+    
+    private RelayCommand? _rowAddCommand;
+    public RelayCommand RowAddCommand =>
+        _rowAddCommand ??= new RelayCommand(AddRow);
+    
+    private void AddRow(object parameter)
+    {
+        var applicationContext = new ApplicationContext();
+        applicationContext.Accounts.Add(new AccountModel
+        {
+            Title = Title,
+            Type = Type,
+            Description = Description,
+            Amount = Amount,
+            UserId = applicationContext.GetUser().Id
+        });
+        
+        applicationContext.SaveChanges();
+    }
 
     public AccountFormViewModel()
     {
