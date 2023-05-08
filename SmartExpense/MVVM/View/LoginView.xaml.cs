0dd4cecbb5436633db4b9e35ce325c51381fe9be
Vitalik Barabash <vitalik.barabash.2002@gmail.com>
2023-05-08 21:50:52 +0300

    Система входу до додатку.

---
+++ SmartExpense/MVVM/View/LoginView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace SmartExpense.MVVM.View;
+
+public partial class LoginView : Window
+{
+    public LoginView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ SmartExpense/MVVM/ViewModel/LoginViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System.Linq;
+using System.Windows;
+using SmartExpense.Core;
+using SmartExpense.MVVM.Model;
+using SmartExpense.MVVM.View;
+
+namespace SmartExpense.MVVM.ViewModel;
+
+public class LoginViewModel : ObservableObject
+{
+    private string _username;
+
+    public string Username
+    {
+        get => _username;
+        set
+        {
+            _username = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _password;
+
+    public string Password
+    {
+        get => _password;
+        set
+        {
+            _password = value;
+            OnPropertyChanged();
+        }
+    }
+
+    // Команда для виклику операції входу.
+    private RelayCommand? _loginCommand;
+
+    public RelayCommand LoginCommand =>
+        _loginCommand ??= new RelayCommand(LoginAction);
+
+    private void LoginAction(object parameter)
+    {
+        var currentUser = new UserModel
+        {
+            Username = Username,
+            Password = Password
+        };
+        var applicationContext = new ApplicationContext();
+        var canLogin = false;
+        
+        foreach (var user in applicationContext.Users)
+        {
+            if (user.Username == currentUser.Username && user.Password == currentUser.Password)
+            {
+                canLogin = true;
+                currentUser = user;
+                break;
+            }
+        }
+
+        if (canLogin)
+        {
+            ApplicationContext.SetUser(currentUser);
+        }
+        else
+        {
+            var result = MessageBox.Show("Такого користувача не існує, бажаєте зареєструватись?", "Помилка входу",
+                MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                applicationContext.Users.Add(currentUser);
+                ApplicationContext.SetUser(currentUser);
+                applicationContext.SaveChanges();
+            }
+            else
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+
+    public LoginViewModel()
+    {
+    }
+}
